3c45477b5249e1a849fb3349a6bb645b096c5b02
Lucas Claudino <lucasclaudino9@gmail.com>
2023-06-01 01:29:59 -0400

    refactore swagger

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,33 +1,20 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import springfox.documentation.builders.ApiInfoBuilder;
-// import springfox.documentation.builders.PathSelectors;
-// import springfox.documentation.builders.RequestHandlerSelectors;
-// import springfox.documentation.spi.DocumentationType;
-// import springfox.documentation.spring.web.plugins.Docket;
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// @EnableSwagger2
-// public class SwaggerConfig {
-
-//     @Bean
-//     public Docket api() {
-//         return new Docket(DocumentationType.SWAGGER_2)
-//                 .select()
-//                 .apis(RequestHandlerSelectors.basePackage("com.example.demo")) // Altere o pacote base para o seu
-//                 .paths(PathSelectors.any())
-//                 .build()
-//                 .apiInfo(apiInfo());
-//     }
-
-//     private ApiInfo apiInfo() {
-//         return new ApiInfoBuilder()
-//                 .title("API Documentation")
-//                 .description("Documentation for your API")
-//                 .version("1.0.0")
-//                 .build();
-//     }
-// }
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
